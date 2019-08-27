4fee3d66bf39481340b1d71f868586ab277d9188
RachelMarotta <kelmarotta@icloud.com>
2019-08-27 12:59:43 -0300

    função decifrar minúscula

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,51 @@
 window.cipher = {
   encode: encode,
   decode: decode,
-  };
+};
 
-function encode(str, offset) {
- let result = '';
+function encode(offset, str) {
+ 
+  let result = "";
 
   for (let i=0; i<str.length; i++) {
-    let cipherForm = ((str.charCodeAt(i) - 65 + offset) % 26) + 65;
-      result += String.fromCharCode(cipherForm);  
-  }   
+
+    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+      let cipherFormUpperCase = ((str.charCodeAt(i) - 65 + offset) % 26) + 65;
+      result += String.fromCharCode(cipherFormUpperCase); 
+
+    } else if (str.charCodeAt(i) >=97 && str.charCodeAt(i) <= 122) {
+    
+      let cipherFormLowerCase = ((str.charCodeAt(i) - 97 + offset) % 26) + 97;
+      result += String.fromCharCode(cipherFormLowerCase);
+    } else {
+      result += String.fromCharCode(str.charCodeAt(i));
+    }
+  }
+
   return result;
 }
 
-function decode(msg, offset) {
- let result = '';
+function decode(offset, msg) {
+ 
+  let result = "";
 
   for (let i=0; i<msg.length; i++) {
-    let decipherForm = ((msg.charCodeAt(i) - 90 - offset) % 26) + 90;
-      result += String.fromCharCode(decipherForm);  
+    
+    if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
+      let decipherFormUpperCase = ((msg.charCodeAt(i) - 90 - offset) % 26) + 90;
+      result += String.fromCharCode(decipherFormUpperCase); 
+
+    } else if (msg.charCodeAt(i) >=97 && msg.charCodeAt(i) <= 122) {
+    
+      let decipherFormLowerCase = ((msg.charCodeAt(i) - 122 - offset) % 26) + 122;
+      result += String.fromCharCode(decipherFormLowerCase);
+    } else {
+      result += String.fromCharCode(msg.charCodeAt(i));
+    }
   }   
   return result;
 }
+
+/* let decipherForm = ((msg.charCodeAt(i) - 90 - offset) % 26) + 90;
+    result += String.fromCharCode(decipherForm);  */
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+document.getElementById("btn-clicar1").addEventListener("click", takeText1);
+
 function takeText1() {
   event.preventDefault();
-    let text = document.getElementById("text1").value;
-    let desloc = Number(document.getElementById("desloc").value);
-    let result = window.cipher.encode(text, desloc);
+  let text = document.getElementById("text1").value;
+  let desloc = Number(document.getElementById("desloc").value);
+  let result = window.cipher.encode(desloc, text);
 
-  document.getElementById("test").innerHTML = `${result}` ;
-  }
+  document.getElementById("textCipher").innerHTML = `${result}` ;
+}
+
+document.getElementById("btn-clicar2").addEventListener("click", takeText2);
 
 function takeText2() {
   event.preventDefault();
-    let text = document.getElementById("text1").value;
-    let desloc = Number(document.getElementById("desloc").value);
-    let result = window.cipher.decode(text, desloc);
+  let text = document.getElementById("text1").value;
+  let desloc = Number(document.getElementById("desloc").value);
+  let result = window.cipher.decode(desloc, text);
 
-  document.getElementById("test").innerHTML = `${result}` ;
+  document.getElementById("textCipher").innerHTML = `${result}`;
 }
